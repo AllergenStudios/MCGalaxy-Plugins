2cb09da211e8ec4b20b619981811719774ab0317
AllergenStudios <128644964+AllergenStudios@users.noreply.github.com>
2024-05-19 16:53:27 -0700

    added a tone indicators plugin!

---
+++ ToneIndicators.cs
@@ -0,0 +1,99 @@
+using System;
+using MCGalaxy;
+using MCGalaxy.Events.ServerEvents;
+using MCGalaxy.Events.PlayerEvents;
+
+
+// REQUIRES THE LATEST VERSION OF MCGALAXY!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+namespace PluginToneIndicators
+{
+    public sealed class ToneIndicators : Plugin 
+    {
+        public override string name { get { return "ToneIndicators"; } }
+        public override string MCGalaxy_Version { get { return "0.1"; } }
+        public override string creator { get { return "AllergenX"; } }
+        
+        public override void Load(bool startup) {
+            OnChatEvent.Register(OnChat, Priority.High);
+        }
+        
+        public override void Unload(bool shutdown) {
+            OnChatEvent.Unregister(OnChat);
+        }
+
+        public static void OnChat(ChatScope scope, Player source, ref string msg, object arg, ref ChatMessageFilter filter, bool relay) {
+            if (msg.CaselessContains("/j"))
+            {
+                const string removeString = "/j";
+                int index = msg.IndexOf(removeString);
+                int length = removeString.Length;
+                string startOfString = msg.Substring(0, index);
+                string endOfString = msg.Substring(index + length);
+                string cleanString = startOfString + endOfString;
+                msg = ("&7[&aJOKING&7] " + cleanString);
+            }
+            else if (msg.CaselessContains("/s"))
+            {
+                const string removeString = "/s";
+                int index = msg.IndexOf(removeString);
+                int length = removeString.Length;
+                string startOfString = msg.Substring(0, index);
+                string endOfString = msg.Substring(index + length);
+                string cleanString = startOfString + endOfString;
+                msg = ("&7[&aSARCASM&7] " + cleanString);
+            }
+            else if (msg.CaselessContains("/hj"))
+            {
+                const string removeString = "/hj";
+                int index = msg.IndexOf(removeString);
+                int length = removeString.Length;
+                string startOfString = msg.Substring(0, index);
+                string endOfString = msg.Substring(index + length);
+                string cleanString = startOfString + endOfString;
+                msg = ("&7[&aHALF-JOKING&7] " + cleanString);
+            }
+            else if (msg.CaselessContains("/srs"))
+            {
+                const string removeString = "/srs";
+                int index = msg.IndexOf(removeString);
+                int length = removeString.Length;
+                string startOfString = msg.Substring(0, index);
+                string endOfString = msg.Substring(index + length);
+                string cleanString = startOfString + endOfString;
+                msg = ("&7[&aSERIOUS&7] " + cleanString);
+            }
+            else if (msg.CaselessContains("/srs"))
+            {
+                const string removeString = "/nsrs";
+                int index = msg.IndexOf(removeString);
+                int length = removeString.Length;
+                string startOfString = msg.Substring(0, index);
+                string endOfString = msg.Substring(index + length);
+                string cleanString = startOfString + endOfString;
+                msg = ("&7[&aNOT SERIOUS&7] " + cleanString);
+            }
+            else if (msg.CaselessContains("/r"))
+            {
+                const string removeString = "/r";
+                int index = msg.IndexOf(removeString);
+                int length = removeString.Length;
+                string startOfString = msg.Substring(0, index);
+                string endOfString = msg.Substring(index + length);
+                string cleanString = startOfString + endOfString;
+                msg = ("&7[&aROMANTIC&7] " + cleanString);
+            }
+            else if (msg.CaselessContains("/t"))
+            {
+                const string removeString = "/t";
+                int index = msg.IndexOf(removeString);
+                int length = removeString.Length;
+                string startOfString = msg.Substring(0, index);
+                string endOfString = msg.Substring(index + length);
+                string cleanString = startOfString + endOfString;
+                msg = ("&7[&aTEASING&7] " + cleanString);
+            }
+        }
+    }
+}
