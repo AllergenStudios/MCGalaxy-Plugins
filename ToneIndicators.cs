8c7d7dd8cd1699cb7837f1c930ae1b2aae891933
AllergenStudios <128644964+AllergenStudios@users.noreply.github.com>
2024-05-19 19:28:04 -0700

    Changes been made and such by Goodly! THANK YOU SO MUCH!

--- ToneIndicators.cs
+++ ToneIndicators.cs
@@ -4,15 +4,19 @@ using MCGalaxy.Events.ServerEvents;
 using MCGalaxy.Events.PlayerEvents;
 
 
-// REQUIRES THE LATEST VERSION OF MCGALAXY!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// REQUIRES THE VERY LATEST VERSION OF MCGALAXY!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// This version has been edited and changed by Goodly. Thank you so much!
 
 
 namespace PluginToneIndicators
 {
     public sealed class ToneIndicators : Plugin 
     {
-        public override string name { get { return "ToneIndicators"; } }
-        public override string MCGalaxy_Version { get { return "0.1"; } }
+        public override string name { get { return "tones"; } }
+
+        public override string MCGalaxy_Version { get { return "1.9.4.9"; } }
+        
         public override string creator { get { return "AllergenX"; } }
         
         public override void Load(bool startup) {
@@ -22,78 +26,48 @@ namespace PluginToneIndicators
         public override void Unload(bool shutdown) {
             OnChatEvent.Unregister(OnChat);
         }
-
+        
         public static void OnChat(ChatScope scope, Player source, ref string msg, object arg, ref ChatMessageFilter filter, bool relay) {
-            if (msg.CaselessContains("/j"))
-            {
-                const string removeString = "/j";
-                int index = msg.IndexOf(removeString);
-                int length = removeString.Length;
-                string startOfString = msg.Substring(0, index);
-                string endOfString = msg.Substring(index + length);
-                string cleanString = startOfString + endOfString;
-                msg = ("&7[&ajoking&7] " + cleanString);
-            }
-            else if (msg.CaselessContains("/s"))
-            {
-                const string removeString = "/s";
-                int index = msg.IndexOf(removeString);
-                int length = removeString.Length;
-                string startOfString = msg.Substring(0, index);
-                string endOfString = msg.Substring(index + length);
-                string cleanString = startOfString + endOfString;
-                msg = ("&7[&asarcasm&7] " + cleanString);
-            }
-            else if (msg.CaselessContains("/hj"))
-            {
-                const string removeString = "/hj";
-                int index = msg.IndexOf(removeString);
-                int length = removeString.Length;
-                string startOfString = msg.Substring(0, index);
-                string endOfString = msg.Substring(index + length);
-                string cleanString = startOfString + endOfString;
-                msg = ("&7[&ahalf-joking&7] " + cleanString);
-            }
-            else if (msg.CaselessContains("/srs"))
-            {
-                const string removeString = "/srs";
-                int index = msg.IndexOf(removeString);
-                int length = removeString.Length;
-                string startOfString = msg.Substring(0, index);
-                string endOfString = msg.Substring(index + length);
-                string cleanString = startOfString + endOfString;
-                msg = ("&7[&aserious&7] " + cleanString);
-            }
-            else if (msg.CaselessContains("/srs"))
-            {
-                const string removeString = "/nsrs";
-                int index = msg.IndexOf(removeString);
-                int length = removeString.Length;
-                string startOfString = msg.Substring(0, index);
-                string endOfString = msg.Substring(index + length);
-                string cleanString = startOfString + endOfString;
-                msg = ("&7[&anot serious&7] " + cleanString);
-            }
-            else if (msg.CaselessContains("/r"))
-            {
-                const string removeString = "/r";
-                int index = msg.IndexOf(removeString);
-                int length = removeString.Length;
-                string startOfString = msg.Substring(0, index);
-                string endOfString = msg.Substring(index + length);
-                string cleanString = startOfString + endOfString;
-                msg = ("&7[&aromantic&7] " + cleanString);
-            }
-            else if (msg.CaselessContains("/t"))
-            {
-                const string removeString = "/t";
-                int index = msg.IndexOf(removeString);
-                int length = removeString.Length;
-                string startOfString = msg.Substring(0, index);
-                string endOfString = msg.Substring(index + length);
-                string cleanString = startOfString + endOfString;
-                msg = ("&7[&ateasing&7] " + cleanString);
-            }
+
+            if (HandleTone("/j", "joking", ref msg)) { return; }
+            if (HandleTone("/s", "sarcasm", ref msg)) { return; }
+            if (HandleTone("/hj", "half-joking", ref msg)) { return; }
+            if (HandleTone("/srs", "serious", ref msg)) { return; }
+            if (HandleTone("/nsrs", "not-serious", ref msg)) { return; }
+            if (HandleTone("/r", "romantic", ref msg)) { return; }
+            if (HandleTone("/t", "teasing", ref msg)) { return; }
+
+        }
+            
+        public static int FindTone(string tone, string msg) {
+            int toneStart = msg.IndexOf(tone);
+            if (toneStart == -1) { return -1; }
+            
+            bool leftSideClear = false;
+            bool rightSideClear = false;
+            
+            if (toneStart == 0                        || msg[toneStart-1]           == ' ') { leftSideClear = true; } 
+            if (toneStart + tone.Length == msg.Length || msg[toneStart+tone.Length] == ' ') { rightSideClear = true; }
+            
+            if (leftSideClear && rightSideClear) { return toneStart; }
+            else { return -1; }
+        }
+        
+        public static bool HandleTone(string tone, string prefix, ref string msg) {
+        
+            int toneStart = FindTone(tone, msg);
+            if (toneStart == -1) { return false; }
+            
+            string startOfString = msg.Substring(0, toneStart);
+            string endOfString = msg.Substring(toneStart + tone.Length);
+            
+            // Remove a space to prevent double spaces when removing a tone
+            if (endOfString.Length > 0 && endOfString[0] == ' ') { endOfString = endOfString.Substring(1); }
+            
+            string cleanString = startOfString + endOfString;
+            
+            msg = ("&7[&a"+prefix+"&7] " + cleanString);
+            return true;
         }
     }
 }
